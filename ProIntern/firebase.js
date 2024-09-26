602b9e1b108ad33bf56936d4c315f1d101ea73e1
Aathif <aathifzahir12@gmail.com>
2024-09-26 16:47:11 +0530

    firebase to main

---
+++ ProIntern/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIFNOYMZ1bP5KWmXv9Rw05DDY8Qrb2Yas",
+  authDomain: "pro-intern.firebaseapp.com",
+  projectId: "pro-intern",
+  storageBucket: "pro-intern.appspot.com",
+  messagingSenderId: "694603024829",
+  appId: "1:694603024829:web:5fab2756cebf925db5aad4",
+  measurementId: "G-7XFPW5BQRB"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
