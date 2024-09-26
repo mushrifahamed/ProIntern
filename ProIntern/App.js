c627b2c9ce154faa0570f88f98893459a3654d40
Aathif <aathifzahir12@gmail.com>
2024-09-26 16:52:32 +0530

    Revert "Reapply "Aathif""
    This reverts commit 6a7e23142d22af1ebf83fc2d5ffd5451089a7680.

--- ProIntern/App.js
+++ ProIntern/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Register_Recruit from './screens/Register_Recruit';
-import Login_Recruit from './screens/Login_Recruit';
-
-
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="LoginRecruit" component={Login_Recruit} />
-        <Stack.Screen name="RegisterRecruit" component={Register_Recruit} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
