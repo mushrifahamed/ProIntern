4385b248390995ba1a3c687a29fae387a0eb9456
Aathif <aathifzahir12@gmail.com>
2024-10-09 17:47:20 +0530

    Update HomeIcon.js

--- assets/icons/HomeIcon.js
+++ assets/icons/HomeIcon.js
@@ -1,15 +1,14 @@
-// HomeIcon.js
 import React from 'react';
 import { SvgXml } from 'react-native-svg';
 
-const HomeIcon = ({ size = 24, strokeColor = "#034694", strokeWidth = 2 }) => {  // Default values for stroke
+const HomeIcon = ({ color = "#034694", strokeWidth = 0, size = 24 }) => {
   const svgMarkup = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="estate">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="${size}" height="${size}">
       <path 
-        fill="#034694" 
-        stroke="${strokeColor}" 
+        fill="${color}" 
+        stroke="${color}" 
         stroke-width="${strokeWidth}" 
-        d="M20,8h0L14,2.74a3,3,0,0,0-4,0L4,8a3,3,0,0,0-1,2.26V19a3,3,0,0,0,3,3H18a3,3,0,0,0,3-3V10.25A3,3,0,0,0,20,8ZM14,20H10V15a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1Zm5-1a1,1,0,0,1-1,1H16V15a3,3,0,0,0-3-3H11a3,3,0,0,0-3,3v5H6a1,1,0,0,1-1-1V10.25a1,1,0,0,1,.34-.75l6-5.25a1,1,0,0,1,1.32,0l6,5.25a1,1,0,0,1,.34.75Z">
+        d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z">
       </path>
     </svg>
   `;
